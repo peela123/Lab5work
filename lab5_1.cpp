65ed00a2f3fa70c00ebb8f55844b9ce865e3df3d
peela123 <peelaxv@gmail.com>
2020-12-15 17:41:40 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+   int integer = 1;
+   int even = 0;
+   int odd = 0;
+   while(integer != 0){
+   cout << "Enter an integer: ";
+   cin >> integer;
+   if(integer%2 == 1)
+   {
+   odd++;
+   }
+   else if(integer == 0)
+   {
+   odd = odd+0;
+   even = even+0;
+   }
+   else
+   {
+   even++;
+   }
+    }
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
