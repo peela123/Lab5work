fd4e2dca127b5ddbccdf34ff26e073a4ff6b7508
peela123 <peelaxv@gmail.com>
2020-12-15 17:45:47 +0700

    lab5_2.cpp completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int integer = 1;
+   int even = 0;
+   int odd = 0;
+   while(integer != 0){
+   cout << "Enter an integer: ";
+   cin >> integer;
+   if(integer%2 == 1)
+   {
+   odd++;
+   }
+   else if(integer == 0)
+   {
+   odd = odd+0;
+   even = even+0;
+   }
+   else
+   {
+   even++;
+   }
+    }
+    cout << "#Even numbers = " << even;
+    cout << "#Odd numbers = " << odd;
+    return 0;
+}
